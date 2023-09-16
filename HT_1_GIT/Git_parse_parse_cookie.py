48095735329ad39997b5768fa9a02ffe891c6809
antar <333antar333@gmail.com>
2023-09-16 14:50:15 +0200

    Added the `parse_cookie` function to the Git_parse_parse_cookie.py file

--- HT_1_GIT/Git_parse_parse_cookie.py
+++ HT_1_GIT/Git_parse_parse_cookie.py
@@ -21,3 +21,23 @@ if __name__ == '__main__':
     assert parse('https://example.com/path/to/page?country=Ukraine&language=Ukrainian') == {'country': 'Ukraine',
                                                                                             'language': 'Ukrainian'}
     assert parse('https://example.com/path/to/page?score=95&subject=Math') == {'score': '95', 'subject': 'Math'}
+
+
+def parse_cookie(query: str) -> dict:
+    cookies = query.split(';')
+    dict_cookie = {cookie[:cookie.find("=")]: cookie[cookie.find("=") + 1:] for cookie in cookies if "=" in cookie}
+    # специально сделана такая мудрёная проверка так как в четвертой проверке стоит два знака "=".
+    return dict_cookie
+
+
+if __name__ == '__main__':
+    assert parse_cookie('name=Viktor;') == {'name': 'Viktor'}
+    assert parse_cookie('') == {}
+    assert parse_cookie('name=Dima;age=28;') == {'name': 'Dima', 'age': '28'}
+    assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}
+    assert parse_cookie('age=25;city=Kharkiv') == {'age': '25', 'city': 'Kharkiv'}
+    assert parse_cookie('animal=dog') == {'animal': 'dog'}
+    assert parse_cookie('fruit=apple;color=red') == {'fruit': 'apple', 'color': 'red'}
+    assert parse_cookie('name=Roman;occupation=programmer') == {'name': 'Roman', 'occupation': 'programmer'}
+    assert parse_cookie('country=Ukraine;language=Ukrainian') == {'country': 'Ukraine', 'language': 'Ukrainian'}
+    assert parse_cookie('score=95;subject=Math') == {'score': '95', 'subject': 'Math'}
